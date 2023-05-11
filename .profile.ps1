aa4dbfe0f20d7e5d47f77091bc62f37fdcb3d5dd
Bino <bonoself@gmail.com>
2023-05-11 16:05:03 +0900

    init

---
+++ .profile.ps1
@@ -0,0 +1,14 @@
+Import-Module QuiteShortAliases
+Import-Module Bino.Bootstrap
+Import-Module Bino.PersonalAliases
+
+function Set-Up {
+    gcloud auth login
+    function Get-GcloudSecret([string] $name) {
+        gcloud secrets versions access latest --secret="$name" --project="bino-personal"
+    }
+    mkdir -p ~/.ssh
+    Get-GcloudSecret "SSH_DEV_KEY" | cat > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
+
+    ngrok config add-authtoken $(Get-GcloudSecret "NGROK_API_TOKEN")
+}
