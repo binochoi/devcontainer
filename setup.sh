9b6c69bb2c08f8472f0773a2fc33bd54585448d1
bino <bonoself@gmail.com>
2023-10-01 04:12:41 +0900

    root 버전 강제

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 # 새 기기 혹은 업그레이드 할 기기의 환경을 세팅한다.
 
+if [ "$USER" != "root" ]; then
+    echo "이 스크립트는 root 권한이 필요함."
+    exit 1
+fi
+
 sudo sh ./installs/pwsh.sh
 sudo pwsh ./scripts/installDeps.ps1 -path ./installs
 sudo pwsh ./scripts/bootstrap.ps1
-sudo mkdir -p /home/bino/.config/powershell/
-sudo cp ./.profile.ps1 /home/bino/.config/powershell/Microsoft.PowerShell_profile.ps1
-sudo chsh -s /opt/microsoft/powershell/7/pwsh bino
+sudo mkdir -p /home/$SUDO_USER/.config/powershell/
+sudo cp ./.profile.ps1 /home/$SUDO_USER/.config/powershell/Microsoft.PowerShell_profile.ps1
+sudo chsh -s /opt/microsoft/powershell/7/pwsh $SUDO_USER
